94924422fa4d9c50878e733c4956cf29f88b1b27
Jon Thallaug <jthallaug@gmail.com>
2020-08-20 20:17:58 +0200

    test

---
+++ main.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+var token = "[insert token here]";
+
+
+
+client.login("NzE5MjMzNzMzMDIyMDU2NTA5.Xt5oUg.d4hubtrmRihtx4du8FIdPEvX4r4");
